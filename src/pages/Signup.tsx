225fdf63947148e1b2df5a24c6b84fb1a7c0c708
Dalbert Joe <dalbertjoe.28ad@licet.ac.in>
2026-04-26 21:14:24 +0530

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -202,7 +202,7 @@ const Signup = () => {
         </div>
 
         <p className="text-center text-[10px] text-muted-foreground/40 mt-6 font-body">
-          Designed by Dalbert Joe
+          Designed by 2MenDevs
         </p>
       </div>
     </div>
